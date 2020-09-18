083f137a894771b3ba942bb34dae0c9818a25f37
rinnegard <victor.rinnegard@hotmail.com>
2020-09-19 00:40:10 +0100

    Login form event

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,22 @@ import React, {useState, useEffect} from 'react';
 import axios from "axios";
 
 function Login() {
+    function formSubmit(e) {
+        e.preventDefault();
+        alert("hello");
+    }
+
     return (
-        <form>
-            <label for="email">Email</label>
-            <input type="email" name="email"/>
-            <label for="password">Password</label>
-            <input type="password" name="password"/>
-            <input type="submit" value="submit" />
-        </form>
+        <div className="content">
+            <form onSubmit={formSubmit}>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" required placeholder="john.doe@gmail.com"/>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" required/>
+                <input className="blue-button button" type="submit" value="Submit" />
+            </form>
+        </div>
+
     )
 }
 
